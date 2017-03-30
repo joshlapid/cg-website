2c9443785f29fc3e4da557dd534f48228cb32b00
Joshua Lapid <lapidjoshua@yahoo.com>
2017-03-30 10:54:06 -0700

    condensed footer

--- index.php
+++ index.php
@@ -1,25 +1,8 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Corrao Group - Optimizing Salesforce</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- Bootstrap CDN's -->
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<?php $title = 'Corrao Group'; ?>
+<!-- Head -->
+<?php include('head.php'); ?>
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-		<!-- Style CSS -->
-		<link rel="stylesheet" type="text/css" href="assets/style.css">
-
-		<!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-
-	</head>
-	<body>
 		<!-- Navigation -->
     <?php include('navbar.php'); ?>
 
