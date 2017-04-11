1d15622e01a4e32f7c33b9f3d40acb3100e44a87
Joshua Lapid <lapidjoshua@yahoo.com>
2017-04-11 09:59:50 -0700

    added google and pardot tracking codes

--- footer.php
+++ footer.php
@@ -72,5 +72,36 @@ $('.parallax-window').parallax();
 </script>
 <script type="text/javascript" src="assets/smooth-scroll.js"></script>
 <script type="text/javascript" src="assets/scrollshow.js"></script>
+
+<!-- Pardot Tracking Code -->
+<script type="text/javascript">
+
+piAId = '53032';
+
+piCId = '1006';
+
+(function() {
+
+function async_load(){
+
+var s = document.createElement('script'); s.type = 'text/javascript';
+
+s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') +
+
+'.pardot.com/pd.js';
+
+var c = document.getElementsByTagName('script')[0];
+
+c.parentNode.insertBefore(s, c);
+
+}
+
+if(window.attachEvent) { window.attachEvent('onload', async_load); }
+
+else { window.addEventListener('load', async_load, false); }
+
+})();
+
+</script>
 </body>
 </html>
--- head.php
+++ head.php
@@ -21,5 +21,16 @@
 		<!-- Font Awesome -->
 		<script src="https://use.fontawesome.com/8beb5e946f.js"></script>
 
+		<!-- Google Tracking Code -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', 'UA-33277929-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 	</head>
 	<body>
